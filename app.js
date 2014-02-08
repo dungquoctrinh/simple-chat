7a4c4540cff6be4181020b16a5a1c6376393c3cb
Tram Lai <tramlaisf@gmail.com>
2014-02-08 00:52:27 -0800

    Add package.json

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ io.configure(function () {
   io.set("transports", ["xhr-polling"]);
   io.set("polling duration", 10);
 });
-
+  
 
 io.sockets.on('connection', function(socket){
   socket.on("setusername", function(name){
